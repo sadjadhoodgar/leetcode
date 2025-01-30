70e08fa0cce5ca8fe30b07fdd617485491bc7ffa
Sadjad Hoodgar <sadjad.hoodgar@ratepay.com>
2025-01-30 11:01:58 +0100

    #238: Product Of Array Except Self.

---
+++ src/main/java/arrays/ProductExceptSelf.java
@@ -0,0 +1,25 @@
+package arrays;
+
+/**
+ * LeetCode Problem #238: Product Of Array Except Self.
+ * Link: <a href="https://leetcode.com/problems/product-of-array-except-self/">...</a>
+ */
+public class ProductExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int suffixProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+
+        return result;
+    }
+}
