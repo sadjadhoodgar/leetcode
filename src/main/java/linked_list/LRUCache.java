c862cfe04240f47209c0b781b909909d204c2430
Sadjad Hoodgar <sadjad.hoodgar@ratepay.com>
2025-02-12 15:49:39 +0100

    #146: LRU cache.

--- src/main/java/linked_list/LRUCache.java
+++ src/main/java/linked_list/LRUCache.java
@@ -1,19 +1,35 @@
 package linked_list;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * LeetCode Problem #146: LRU cache.
  * Link: <a href="https://leetcode.com/problems/lru-cache/">...</a>
  */
 public class LRUCache {
-    public LRUCache(int capacity) {
+    int freeCapacity;
+    int lowest = 0;
+    Map<Integer, Integer> cache = new HashMap<>();
 
+    public LRUCache(int capacity) {
+        freeCapacity = capacity;
     }
 
     public int get(int key) {
-
+        return cache.getOrDefault(key, -1);
     }
 
     public void put(int key, int value) {
+        if (freeCapacity > 0) {
+            freeCapacity--;
+        }
 
+        if (value < cache.get(lowest)) {
+            lowest = key;
+        }
+
+
+        cache.put(key, value);
     }
 }
