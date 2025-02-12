795bbd6f66192f01ced5b9d5e687d561e45befb8
Sadjad Hoodgar <sadjad.hoodgar@ratepay.com>
2025-02-12 11:55:09 +0100

    #146: LRU cache.

---
+++ src/main/java/linked_list/LRUCache.java
@@ -0,0 +1,19 @@
+package linked_list;
+
+/**
+ * LeetCode Problem #146: LRU cache.
+ * Link: <a href="https://leetcode.com/problems/lru-cache/">...</a>
+ */
+public class LRUCache {
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
